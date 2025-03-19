68e3a28361510963b1c17bfd6c72bd7f4482f603
Pltyps <166666186+Pltyps@users.noreply.github.com>
2025-03-18 20:43:48 -0600

    Initial Commit

---
+++ backend/Mission11_Manirajan.API/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11_Manirajan.API.Data;
+
+namespace Mission11_Manirajan.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+        public BookController(BookDbContext temp) => _bookContext = temp;
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _bookContext.Books.ToList();
+        }
+    }
+}
