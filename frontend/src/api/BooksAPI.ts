41586a2a9aac881174ebfe4eae74602c962f90ab
Pltyps <166666186+Pltyps@users.noreply.github.com>
2025-04-02 18:38:34 -0600

    fixed frontend API URL

--- frontend/src/api/BooksAPI.ts
+++ frontend/src/api/BooksAPI.ts
@@ -7,7 +7,7 @@ interface FetchBooksResponse {
 }
 
 const API_URL =
-  'https://mission13-manirajan-backend-eya3g0crbuhzbxbm.eastus-01.azurewebsites.net/api/book';
+  'https://mission13-manirajan-backend-eya3g0crbuhzbxbm.eastus-01.azurewebsites.net/api';
 
 export const fetchBooks = async (
   pageSize: number,
